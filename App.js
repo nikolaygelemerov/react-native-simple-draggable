acad2a90abc90094ef51de2684af5813314f1fc1
Nikolay Gelemerov <nikolaygelemerov@gmail.com>
2018-02-15 02:07:13 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  PanResponder
+} from 'react-native';
+
+export default class animatedbasic extends Component {
+  
+  componentWillMount() {
+
+      //this will take care of x and  y values
+      this.animatedValue = new Animated.ValueXY();
+      this._value = {x: 0, y: 0}
+
+      //update the value
+      this.animatedValue.addListener(value => this._value = value);
+
+      //create our panResponder
+      this.panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: (e, gestureState) => true,
+        onMoveShouldSetPanResponder: (e, gestureState) => true,
+        onPanResponderGrant: (e, gestureState) => {
+          this.animatedValue.setOffset({
+            x: this._value.x,
+            y: this._value.y,
+          })
+          this.animatedValue.setValue({ x: 0, y: 0})
+        },
+        onPanResponderMove: Animated.event([
+          null, { dx: this.animatedValue.x, dy: this.animatedValue.y}
+        ]),
+        onPanResponderRelease: (e, gestureState) => {
+          this.animatedValue.flattenOffset();
+
+          //handle decay on dragend
+          Animated.decay(this.animatedValue, {
+            deceleration: 0.997,
+            velocity: { x: gestureState.vx, y: gestureState.vy }
+          }).start();
+        },
+      })
+    }
+  
+  render() {
+      const animatedStyle = {
+        transform: this.animatedValue.getTranslateTransform()
+      }
+      return (
+        <View style={styles.container}>
+          <Animated.View style={[styles.box, animatedStyle]} {...this.panResponder.panHandlers}>
+            <Text style={styles.text}>Drag Me</Text>
+          </Animated.View>
+        </View>
+      );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  box: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    backgroundColor: '#333',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    color: '#FFF',
+    fontSize: 20
+  }
+});
+
+AppRegistry.registerComponent('animatedbasic', () => animatedbasic);
